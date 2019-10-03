81a6bd979b491e783ea97719dcf17449ca9f4509
Thomas Tuegel <ttuegel@mailbox.org>
2019-10-03 12:04:26 -0500

    Use niv to track Nixpkgs

--- release.nix
+++ release.nix
@@ -1,22 +1,17 @@
 let
-  src = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
-  nixpkgs = builtins.fetchTarball {
-    url = src.url + "/archive/${src.rev}.tar.gz";
-    inherit (src) sha256;
-  };
-  pkgs = import nixpkgs {
-    overlays = [ (import ./.) ];
-  };
+  sources = import ./nix/sources.nix;
+  overlay = _: pkgs: { niv = import sources."niv" {}; };
+  nixpkgs = import sources."nixpkgs" { overlays = [ overlay (import ./.) ]; config = {}; };
 in
 
 # The set of all packages provided by this overlay, for testing purposes; does
 # not include transitive dependencies provided by Nixpkgs, which should be
 # tested upstream.
 {
-  inherit (pkgs) k mvnix secp256k1 z3 z3_6_0 z3_4_8;
-  haskellPackages = pkgs.haskell.packages.stackage.lts_12_21;
-  ocamlPackages_4_06_k = pkgs.recurseIntoAttrs {
-    inherit (pkgs.ocamlPackages_4_06_k)
+  inherit (nixpkgs) niv k mvnix secp256k1 z3 z3_6_0 z3_4_8;
+  haskellPackages = nixpkgs.haskell.packages.stackage.lts_12_21;
+  ocamlPackages_4_06_k = nixpkgs.recurseIntoAttrs {
+    inherit (nixpkgs.ocamlPackages_4_06_k)
       base bn128 configurator mlgmp ocaml ocaml-protoc ppx_deriving_protobuf
       secp256k1 stdio;
   };
